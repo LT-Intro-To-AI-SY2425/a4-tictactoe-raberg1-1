f4f45d5712eb0684d8731ab07c53501827a64d6a
Robert Berg <rberg@cps.edu>
2024-10-30 11:36:32 -0500

    updated notes - day 1

--- notes.py
+++ notes.py
@@ -2,3 +2,20 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    def __init__(self, n = "no name", a = 0):
+        self.name = n
+        self.age = a
+
+    def __str__(self) -> str:
+        s = f"{self.name} is {self.age} years old"
+        return s
+
+# instances of the Dog class
+logan = Dog("logan", 8)
+cookie = Dog()
+maple = Dog("maple", 1)
+
+print(logan)
+print(cookie)
+print(maple)
